8908ad3c1aecd26cc7dec0d2af600372d6fa9608
Aryan Sharma <aryan6002261@gmail.com>
2026-07-09 17:15:13 +0530

    Refining Module 4

--- module-4.js
+++ module-4.js
@@ -1,6 +1,12 @@
+// ===============================
+// Module 4/20 - JavaScript Basics
+// Topic: Conditional Statements
+// ===============================
+
 let a = 100;
 let b = 200;
 
+// if, else if, else
 if (a > b) {
     console.log("a is greater than b");
 } else if (a < b) {
@@ -9,7 +15,8 @@ if (a > b) {
     console.log("a is equal to b");
 }
 
-let falseValues = [
+// Falsy values
+const falsyValues = [
     false,
     0,
     -0,
@@ -18,9 +25,19 @@ let falseValues = [
     null,
     undefined,
     NaN
-]
+];
 
-let trueValues = [
+console.log("\nFalsy Values:");
+for (const value of falsyValues) {
+    if (value) {
+        console.log(value, "is Truthy");
+    } else {
+        console.log(value, "is Falsy");
+    }
+}
+
+// Truthy values
+const truthyValues = [
     true,
     1,
     -1,
@@ -29,5 +46,34 @@ let trueValues = [
     "false",
     [],
     {},
-    function() {}
-]
+    function () {}
+];
+
+console.log("\nTruthy Values:");
+for (const value of truthyValues) {
+    if (value) {
+        console.log(value, "is Truthy");
+    } else {
+        console.log(value, "is Falsy");
+    }
+}
+
+// Nested if example
+let age = 20;
+let hasID = true;
+
+console.log("\nNested If:");
+
+if (age >= 18) {
+    if (hasID) {
+        console.log("Access granted.");
+    } else {
+        console.log("Please show a valid ID.");
+    }
+} else {
+    console.log("Access denied. You must be at least 18 years old.");
+}
+
+// Ternary operator
+let result = age >= 18 ? "Adult" : "Minor";
+console.log("\nTernary Result:", result);
